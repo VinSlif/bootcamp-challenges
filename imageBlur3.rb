70cf090182d3099d694a03289c0672f823030d29
Vincent Slifer <Vincent.Slifer@gmail.com>
2019-07-24 21:56:44 +0000

    Fixed blur to fully work as intended

--- imageBlur3.rb
+++ imageBlur3.rb
@@ -4,35 +4,40 @@ class  Image
 	end
 	
 	def blur(n)
-		(1..n).each { |i| self.transform }
+		(1..n).each { self.transform }
 	end
 
 	def transform
-		out = []
-		len = @img[0].length
-		# gets indexes of all 1s in array
-		@img.each_index {|i| j = @img[i].index 1; out << [i, j] if j}
+		out = Marshal.load(Marshal.dump(@img))
+		leny = @img.length - 1
+		lenx = @img[0].length - 1
 
-		out.each do |px|
-			if px[0] - 1 >= 0# above
-				@img[px[0] - 1][px[1]] = 1
-			end
-			if px[0] + 1 < len # below
-				@img[px[0] + 1][px[1]] = 1
-			end
-			if px[1] - 1 >= 0 # left
-				@img[px[0]][px[1] - 1] = 1
-			end
-			if px[1] + 1 < len # right
-				@img[px[0]][px[1] + 1] = 1
+		@img.each_index do |i|
+			@img[i].each_index do |j|
+				next if @img[i][j] == 0
+
+				if i > 0 # above
+					out[i - 1][j] = 1
+				end
+				if j > 0 # left
+					out[i][j - 1] = 1
+				end
+
+				if i < leny # below
+					out[i + 1][j] = 1
+				end
+				if j < lenx # right
+					out[i][j + 1] = 1
+				end
 			end
 		end
+		@img = out
 	end
 
 	def output_image
 		out = []
 		for i in @img
-			out << i.join
+			out << i.join(" ")
 		end
 		puts out
 	end
@@ -46,7 +51,7 @@ image = Image.new([
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 1, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[1, 0, 0, 0, 0, 0, 0, 0, 1],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0],
